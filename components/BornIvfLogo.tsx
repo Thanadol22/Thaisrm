8ec064c0ebab61146ca007e4a0296e0bab369107
Thanadol Jampatem <Thanadolpetch22@gmail.com>
2026-09-14 16:18:26 +0700

    refactor(cleanup): remove deprecated legacy components (BornIvfLogo, OfficialGeneticsWorkshopAgenda, StaffTopNavbar)

--- components/BornIvfLogo.tsx
+++
@@ -1,2 +0,0 @@
-import React from 'react';
-export { ThaiSrmLogo, ThaisrmLogo, BornIvfLogo } from './ThaiSrmLogo';
--- components/OfficialGeneticsWorkshopAgenda.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import React from 'react';
-import { OfficialWorkshopAgenda } from './OfficialWorkshopAgenda';
-
-export function OfficialGeneticsWorkshopAgenda(props: { onPrint?: () => void; lang?: string }) {
-  return <OfficialWorkshopAgenda {...props} />;
-}
