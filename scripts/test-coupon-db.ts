508b303d657df6560bfdd77880b9e8b5fdc61eba
Thanadol Jampatem <Thanadolpetch22@gmail.com>
2026-09-24 16:22:53 +0700

    chore: remove unused test scripts and clean up repository

--- scripts/test-coupon-db.ts
+++
@@ -1,8 +0,0 @@
-import prisma from '../lib/prisma';
-
-async function main() {
-  const coupons = await (prisma as any).coupons.findMany();
-  console.log(JSON.stringify(coupons, null, 2));
-}
-
-main().catch(console.error);
